75b8162c3263061ab4e6a3e20daddcd24879a173
Daniel Šádek <me@danielsadek.cz>
2021-06-15 15:56:54 +0200

    Autoload app locale

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -13,7 +13,7 @@ class FieldServiceProvider extends ServiceProvider
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-opening-hours-field', __DIR__ . '/../dist/js/field.js');
             Nova::style('nova-opening-hours-field', __DIR__ . '/../dist/css/field.css');
-            Nova::translations(__DIR__ . '/../resources/lang/en.json');
+            Nova::translations(__DIR__ . '/../resources/lang/' . app()->getLocale() . '.json');
         });
     }
 
